3edc3b2c11fd6e31e2f410a08c6ffcd7a6131a2f
amanda <amanda.blaze1988@gmail.com>
2021-11-29 17:27:33 -0800

    Created route for Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :services
+  resources :users, only: [:create]
+  resources :services, only: [:index, :show]
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
